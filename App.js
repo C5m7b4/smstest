f133a6ac93153872174a9ae0f8b3548a3a40726a
C5m7b4 <mbedingfield@dcrpos.com>
2022-04-04 10:26:17 -0500

    remove unused

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Home from './src/Home';
 
 export default function App() {
@@ -16,7 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#34a1eb',
     alignItems: 'center',
-    //justifyContent: 'center',
     textAlign: 'center',
     paddingTop: 70,
     width: '100%',
